26a66495b3c5deaab948d129a2f2c202f2d0d5f5
99061142 <99061142@mydavinci.nl>
2022-04-11 17:20:22 +0200

    Test file

---
+++ robotarm/exercises/exercise1.py
@@ -0,0 +1,13 @@
+from RobotArm import SmartRobotArm
+
+robotArm = SmartRobotArm()
+robotArm.loadMyLevel([['yellow, yellow, blue', 'blue'], [], []])
+
+# Jouw python instructies zet je vanaf hier:
+
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
